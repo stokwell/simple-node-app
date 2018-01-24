83b3d9668d6569bc366b9127d32f013fb86e010f
Vitaly Lyapunov <stokwell1@gmail.com>
2018-01-24 16:54:59 +0100

    first commit

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const home = require('./home');
+const public = require('./public');
+const search = require('./search');
+const notFound = require('./notFound');
+
+module.exports = {
+  public,
+  home,
+  search,
+  notFound
+}
